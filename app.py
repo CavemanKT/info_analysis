ac864ef1dbefbb91b3f66aae0240b549c491ec75
kt-imimr <kt@imimr.net>
2024-03-20 12:32:31 +0800

    updated sampleData

--- app.py
+++ app.py
@@ -11,11 +11,21 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 # app = Flask(__name__)
 
-with pdfplumber.open('webDev.pdf') as pdf:
+# check a file if it is a pdf
+def is_pdf(file_path):
+    """
+    Check if a file is a pdf
+    """
+    with open(file_path, 'rb') as f:
+        return f.read(4) == b'%PDF'
+
+isPdfType = is_pdf('test.pdf')
+print("🐍 File: info_analysis/app.py | Line: 23 | undefined ~ pdfType",isPdfType)
+
+with pdfplumber.open('test.pdf') as pdf:
     text_content = ""
     for page in pdf.pages:
         text_content += page.extract_text()
-    print(text_content)
 
 words = text_content.split()  # Split text into individual words
 word_frequency = Counter(words)
@@ -24,8 +34,16 @@ word_frequency = Counter(words)
 nlp = spacy.load('en_core_web_sm')
 
 # nlp = en_core_web_sm.load()
+max_similarity = 0
+for word in words:
+    doc = nlp(word)
+    searchWord = nlp("sed")
+    similarity = searchWord.similarity(doc)
+    if similarity > max_similarity:
+        max_similarity = similarity
+        print("🐍 File: python/app.py | Line: 31 | undefined ~ similarity",similarity, word)
 
-word1 = nlp('word')
-word2 = nlp('word')
-similarity = word1.similarity(word2)
-print("🐍 File: python/app.py | Line: 31 | undefined ~ similarity",similarity)
+# word1 = nlp('lorem')
+# word2 = nlp('word')
+# similarity = word1.similarity(word2)
+# print("🐍 File: python/app.py | Line: 31 | undefined ~ similarity",similarity)
